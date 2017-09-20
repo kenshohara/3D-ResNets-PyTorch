68aa51904e613d5473e2617299f271411adb2396
Kensho Hara <kensho.hara@aist.go.jp>
2017-09-20 20:27:19 +0900

    remove unused options

--- model.py
+++ model.py
@@ -1,11 +1,11 @@
 import torch
 from torch import nn
 
-from models import resnet, pre_act_resnet, wide_resnet, resnext, densenet
+from models import resnet
 
 
 def generate_model(opt):
-    assert opt.model in ['resnet', 'preresnet', 'wideresnet', 'resnext', 'densenet']
+    assert opt.model in ['resnet']
 
     if opt.model == 'resnet':
         assert opt.model_depth in [18, 34, 50, 101]
@@ -18,39 +18,6 @@ def generate_model(opt):
             model = resnet.resnet50(num_classes=opt.n_classes, shortcut_type=opt.resnet_shortcut)
         elif opt.model_depth == 101:
             model = resnet.resnet101(num_classes=opt.n_classes, shortcut_type=opt.resnet_shortcut)
-    elif opt.model == 'wideresnet':
-        assert opt.model_depth in [50]
-
-        if opt.model_depth == 50:
-            model = wide_resnet.resnet50(num_classes=opt.n_classes, shortcut_type=opt.resnet_shortcut, k=opt.wide_resnet_k)
-    elif opt.model == 'resnext':
-        assert opt.model_depth in [50, 101]
-        if opt.model_depth == 50:
-            model = resnext.resnet50(num_classes=opt.n_classes, shortcut_type=opt.resnet_shortcut, cardinality=opt.resnext_cardinality)
-        elif opt.model_depth == 101:
-            model = resnext.resnet101(num_classes=opt.n_classes, shortcut_type=opt.resnet_shortcut, cardinality=opt.resnext_cardinality)
-    elif opt.model == 'preresnet':
-        assert opt.model_depth in [18, 34, 50, 101]
-
-        if opt.model_depth == 18:
-            model = pre_act_resnet.resnet18(num_classes=opt.n_classes, shortcut_type=opt.resnet_shortcut)
-        elif opt.model_depth == 34:
-            model = pre_act_resnet.resnet34(num_classes=opt.n_classes, shortcut_type=opt.resnet_shortcut)
-        elif opt.model_depth == 50:
-            model = pre_act_resnet.resnet50(num_classes=opt.n_classes, shortcut_type=opt.resnet_shortcut)
-        elif opt.model_depth == 101:
-            model = pre_act_resnet.resnet101(num_classes=opt.n_classes, shortcut_type=opt.resnet_shortcut)
-    elif opt.model == 'densenet':
-        assert opt.model_depth in [121, 169, 201, 161]
-
-        if opt.model_depth == 121:
-            model = densenet.densenet121(num_classes=opt.n_classes)
-        elif opt.model_depth == 169:
-            model = densenet.densenet169(num_classes=opt.n_classes)
-        elif opt.model_depth == 201:
-            model = densenet.densenet201(num_classes=opt.n_classes)
-        elif opt.model_depth == 161:
-            model = densenet.densenet161(num_classes=opt.n_classes)
 
     if not opt.no_cuda:
         model = model.cuda()
