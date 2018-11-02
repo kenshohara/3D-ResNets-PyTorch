79d16ee8141f952aa3f69118fb3b547245dcb85b
Kensho Hara <kensho.hara@aist.go.jp>
2018-11-02 16:24:41 +0900

    remove comment

--- temporal_transforms.py
+++ temporal_transforms.py
@@ -19,14 +19,6 @@ class LoopPadding(object):
 
 
 class TemporalBeginCrop(object):
-    """Temporally crop the given frame indices at a beginning.
-
-    If the number of frames is less than the size,
-    loop the indices as many times as necessary to satisfy the size.
-
-    Args:
-        size (int): Desired output size of the crop.
-    """
 
     def __init__(self, size):
         self.size = size
@@ -43,25 +35,11 @@ class TemporalBeginCrop(object):
 
 
 class TemporalCenterCrop(object):
-    """Temporally crop the given frame indices at a center.
-
-    If the number of frames is less than the size,
-    loop the indices as many times as necessary to satisfy the size.
-
-    Args:
-        size (int): Desired output size of the crop.
-    """
 
     def __init__(self, size):
         self.size = size
 
     def __call__(self, frame_indices):
-        """
-        Args:
-            frame_indices (list): frame indices to be cropped.
-        Returns:
-            list: Cropped frame indices.
-        """
 
         center_index = len(frame_indices) // 2
         begin_index = max(0, center_index - (self.size // 2))
@@ -78,25 +56,11 @@ class TemporalCenterCrop(object):
 
 
 class TemporalRandomCrop(object):
-    """Temporally crop the given frame indices at a random location.
-
-    If the number of frames is less than the size,
-    loop the indices as many times as necessary to satisfy the size.
-
-    Args:
-        size (int): Desired output size of the crop.
-    """
 
     def __init__(self, size):
         self.size = size
 
     def __call__(self, frame_indices):
-        """
-        Args:
-            frame_indices (list): frame indices to be cropped.
-        Returns:
-            list: Cropped frame indices.
-        """
 
         rand_end = max(0, len(frame_indices) - self.size - 1)
         begin_index = random.randint(0, rand_end)
