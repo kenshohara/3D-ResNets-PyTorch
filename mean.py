1bb40d6a90361468c2dc4e0c0ef591db89ba68b9
Kensho Hara <kensho.hara@aist.go.jp>
2018-11-16 19:10:04 +0900

    update mean values

--- mean.py
+++ mean.py
@@ -5,9 +5,8 @@ def get_mean_std(value_scale, dataset):
         mean = [0.4477, 0.4209, 0.3906]
         std = [0.2767, 0.2695, 0.2714]
     elif dataset == 'kinetics':
-        # Kinetics (10 videos for each class)
-        mean = [0.4339, 0.4046, 0.3776]
-        std = [0.1512, 0.1486, 0.1570]
+        mean = [0.4345, 0.4051, 0.3775]
+        std = [0.2768, 0.2713, 0.2737]
 
     mean = [x * value_scale for x in mean]
     std = [x * value_scale for x in std]
