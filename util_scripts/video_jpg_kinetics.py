ed7051862fc5798e9550120e45ffb2927155ea29
Kensho Hara <kensho.hara@aist.go.jp>
2018-11-26 16:43:39 +0900

    refactoring

--- util_scripts/video_jpg_kinetics.py
+++ util_scripts/video_jpg_kinetics.py
@@ -1,48 +1,64 @@
-from __future__ import print_function, division
-import os
 import sys
 import subprocess
+from pathlib import Path
 
-def class_process(dir_path, dst_dir_path, class_name):
-  class_path = os.path.join(dir_path, class_name)
-  if not os.path.isdir(class_path):
-    return
 
-  dst_class_path = os.path.join(dst_dir_path, class_name)
-  if not os.path.exists(dst_class_path):
-    os.mkdir(dst_class_path)
+def class_process(class_dir_path, dst_root_path, fps=-1):
+    if not class_dir_path.is_dir():
+        return
 
-  for file_name in os.listdir(class_path):
-    if '.mp4' not in file_name:
-      continue
-    name, ext = os.path.splitext(file_name)
-    dst_directory_path = os.path.join(dst_class_path, name)
+    dst_class_path = dst_root_path / class_dir_path.name
+    dst_class_path.mkdir(exist_ok=True)
 
-    video_file_path = os.path.join(class_path, file_name)
-    try:
-      if os.path.exists(dst_directory_path):
-        if not os.path.exists(os.path.join(dst_directory_path, 'image_00001.jpg')):
-          subprocess.call('rm -r \"{}\"'.format(dst_directory_path), shell=True)
-          print('remove {}'.format(dst_directory_path))
-          os.mkdir(dst_directory_path)
+    for video_file_path in sorted(class_dir_path.iterdir()):
+        if '.mp4' not in video_file_path.name:
+            continue
+        name = video_file_path.stem
+        dst_dir_path = dst_class_path / name
+
+        if dst_dir_path.exists():
+            continue
         else:
-          continue
-      else:
-        os.mkdir(dst_directory_path)
-    except:
-      print(dst_directory_path)
-      continue
-    cmd = 'ffmpeg -i \"{}\" -vf scale=-1:240 \"{}/image_%05d.jpg\"'.format(video_file_path, dst_directory_path)
-    print(cmd)
-    subprocess.call(cmd, shell=True)
-    print('\n')
+            dst_dir_path.mkdir()
 
-if __name__=="__main__":
-  dir_path = sys.argv[1]
-  dst_dir_path = sys.argv[2]
+        p = subprocess.Popen(
+            'ffprobe -hide_banner -show_entries stream=width,height "{}"'.
+            format(video_file_path),
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE)
+        res = p.communicate()[0].decode('utf-8').split('\n')
+        if len(res) <= 3:
+            continue
+        width = int([x.split('=') for x in res if 'width' in x][0][1])
+        height = int([x.split('=') for x in res if 'height' in x][0][1])
 
-  for class_name in os.listdir(dir_path):
+        if width > height:
+            scale_param = '-1:240'
+        else:
+            scale_param = '240:-1'
+
+        fps_param = ''
+        if fps > 0:
+            fps_param = ',fps={}'.format(fps)
+
+        cmd = 'ffmpeg -i \"{}\" -vf "scale={}{}" \"{}/image_%05d.jpg\"'.format(
+            video_file_path, scale_param, fps_param, dst_dir_path)
+
+        print(cmd)
+        subprocess.call(cmd, shell=True)
+        print('\n')
+
+
+if __name__ == '__main__':
+    dir_path = Path(sys.argv[1])
+    dst_dir_path = Path(sys.argv[2])
+    fps = -1
+    if len(sys.argv) > 3:
+        fps = int(sys.argv[3])
+
+    for class_dir_path in sorted(dir_path.iterdir()):
+        class_process(class_dir_path, dst_dir_path, fps)
+
+    class_name = 'test'
     class_process(dir_path, dst_dir_path, class_name)
-
-  class_name = 'test'
-  class_process(dir_path, dst_dir_path, class_name)
