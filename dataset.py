8fe3155b4192f909c7a18feeaaaefc2058f5d1e4
kenshohara <kensho.hara@aist.go.jp>
2019-07-04 16:13:34 +0900

    reformat file by yapf

--- dataset.py
+++ dataset.py
@@ -9,21 +9,19 @@ def get_training_set(video_path, annotation_path, dataset_name,
     ]
 
     if dataset_name == 'activitynet':
-        training_data = ActivityNet(
-            video_path,
-            annotation_path,
-            'training',
-            spatial_transform=spatial_transform,
-            temporal_transform=temporal_transform,
-            target_transform=target_transform)
+        training_data = ActivityNet(video_path,
+                                    annotation_path,
+                                    'training',
+                                    spatial_transform=spatial_transform,
+                                    temporal_transform=temporal_transform,
+                                    target_transform=target_transform)
     else:
-        training_data = VideoDataset(
-            video_path,
-            annotation_path,
-            'training',
-            spatial_transform=spatial_transform,
-            temporal_transform=temporal_transform,
-            target_transform=target_transform)
+        training_data = VideoDataset(video_path,
+                                     annotation_path,
+                                     'training',
+                                     spatial_transform=spatial_transform,
+                                     temporal_transform=temporal_transform,
+                                     target_transform=target_transform)
 
     return training_data
 
@@ -39,13 +37,12 @@ def get_validation_set(video_path, annotation_path, dataset_name,
                                       spatial_transform, temporal_transform,
                                       target_transform)
     else:
-        validation_data = VideoDataset(
-            video_path,
-            annotation_path,
-            'validation',
-            spatial_transform=spatial_transform,
-            temporal_transform=temporal_transform,
-            target_transform=target_transform)
+        validation_data = VideoDataset(video_path,
+                                       annotation_path,
+                                       'validation',
+                                       spatial_transform=spatial_transform,
+                                       temporal_transform=temporal_transform,
+                                       target_transform=target_transform)
 
     return validation_data
 
@@ -64,21 +61,19 @@ def get_test_set(video_path, annotation_path, dataset_name, test_subset,
     elif test_subset == 'test':
         subset = 'testing'
     if dataset_name == 'activitynet':
-        test_data = ActivityNet(
-            video_path,
-            annotation_path,
-            subset,
-            spatial_transform,
-            temporal_transform,
-            target_transform,
-            is_untrimmed_setting=True)
+        test_data = ActivityNet(video_path,
+                                annotation_path,
+                                subset,
+                                spatial_transform,
+                                temporal_transform,
+                                target_transform,
+                                is_untrimmed_setting=True)
     else:
-        test_data = VideoDataset(
-            video_path,
-            annotation_path,
-            subset,
-            spatial_transform=spatial_transform,
-            temporal_transform=temporal_transform,
-            target_transform=target_transform)
+        test_data = VideoDataset(video_path,
+                                 annotation_path,
+                                 subset,
+                                 spatial_transform=spatial_transform,
+                                 temporal_transform=temporal_transform,
+                                 target_transform=target_transform)
 
     return test_data
