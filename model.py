4db995fe46e8c9b054578c956efaa8d9b8ed93d5
kenshohara <kensho.hara@aist.go.jp>
2019-12-26 11:46:09 +0900

    add n_input_channels into arguments that I forgot in the previous commit.

--- model.py
+++ model.py
@@ -40,6 +40,7 @@ def generate_model(opt):
     if opt.model == 'resnet':
         model = resnet.generate_model(model_depth=opt.model_depth,
                                       n_classes=opt.n_classes,
+                                      n_input_channels=opt.n_input_channels,
                                       shortcut_type=opt.resnet_shortcut,
                                       conv1_t_size=opt.conv1_t_size,
                                       conv1_t_stride=opt.conv1_t_stride,
@@ -48,37 +49,44 @@ def generate_model(opt):
     elif opt.model == 'resnet2p1d':
         model = resnet2p1d.generate_model(model_depth=opt.model_depth,
                                           n_classes=opt.n_classes,
+                                          n_input_channels=opt.n_input_channels,
                                           shortcut_type=opt.resnet_shortcut,
                                           conv1_t_size=opt.conv1_t_size,
                                           conv1_t_stride=opt.conv1_t_stride,
                                           no_max_pool=opt.no_max_pool,
                                           widen_factor=opt.resnet_widen_factor)
     elif opt.model == 'wideresnet':
-        model = wide_resnet.generate_model(model_depth=opt.model_depth,
-                                           k=opt.wide_resnet_k,
-                                           n_classes=opt.n_classes,
-                                           shortcut_type=opt.resnet_shortcut,
-                                           conv1_t_size=opt.conv1_t_size,
-                                           conv1_t_stride=opt.conv1_t_stride,
-                                           no_max_pool=opt.no_max_pool)
+        model = wide_resnet.generate_model(
+            model_depth=opt.model_depth,
+            k=opt.wide_resnet_k,
+            n_classes=opt.n_classes,
+            n_input_channels=opt.n_input_channels,
+            shortcut_type=opt.resnet_shortcut,
+            conv1_t_size=opt.conv1_t_size,
+            conv1_t_stride=opt.conv1_t_stride,
+            no_max_pool=opt.no_max_pool)
     elif opt.model == 'resnext':
         model = resnext.generate_model(model_depth=opt.model_depth,
                                        cardinality=opt.resnext_cardinality,
                                        n_classes=opt.n_classes,
+                                       n_input_channels=opt.n_input_channels,
                                        shortcut_type=opt.resnet_shortcut,
                                        conv1_t_size=opt.conv1_t_size,
                                        conv1_t_stride=opt.conv1_t_stride,
                                        no_max_pool=opt.no_max_pool)
     elif opt.model == 'preresnet':
-        model = pre_act_resnet.generate_model(model_depth=opt.model_depth,
-                                              n_classes=opt.n_classes,
-                                              shortcut_type=opt.resnet_shortcut,
-                                              conv1_t_size=opt.conv1_t_size,
-                                              conv1_t_stride=opt.conv1_t_stride,
-                                              no_max_pool=opt.no_max_pool)
+        model = pre_act_resnet.generate_model(
+            model_depth=opt.model_depth,
+            n_classes=opt.n_classes,
+            n_input_channels=opt.n_input_channels,
+            shortcut_type=opt.resnet_shortcut,
+            conv1_t_size=opt.conv1_t_size,
+            conv1_t_stride=opt.conv1_t_stride,
+            no_max_pool=opt.no_max_pool)
     elif opt.model == 'densenet':
         model = densenet.generate_model(model_depth=opt.model_depth,
                                         n_classes=opt.n_classes,
+                                        n_input_channels=opt.n_input_channels,
                                         conv1_t_size=opt.conv1_t_size,
                                         conv1_t_stride=opt.conv1_t_stride,
                                         no_max_pool=opt.no_max_pool)
